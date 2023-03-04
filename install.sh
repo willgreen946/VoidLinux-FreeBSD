a3496429feeb4a24a5b54df28467812a11c707ea
wrwrwrwrwr <72352225+willrollitt@users.noreply.github.com>
2023-03-04 12:50:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 # Installing all packages I use
-exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr bash zathura x11-drivers/xf86-video-intel 
+exec pkg install doas vim vifm lynx git xorg neofetch firefox feh mpv wifimgr bash zathura ubuntu-font x11-drivers/xf86-video-intel 
 
 # This is for better intel graphics
-
 add kern.vty=vt to /boot/loader.conf
 
 # Adding bash as default shell for my user
