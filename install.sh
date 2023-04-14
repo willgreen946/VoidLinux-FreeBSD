2ddc904d38566c780da8367c2bd632421689c227
ZK <72352225+willrollitt@users.noreply.github.com>
2023-04-14 11:26:17 +0100

    changed graphics settings

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 # Installing all packages I use
-exec pkg install doas vim vifm lynx git xorg neofetch scrot feh ungoogled-chromium mpv wifimgr bash zathura ubuntu-font picom audio/mixertui x11-fonts/nerd-fonts x11-drivers/xf86-video-intel 
+exec pkg install doas vim vifm lynx git xorg neofetch scrot drm-kmod feh ungoogled-chromium mpv wifimgr bash zathura ubuntu-font picom audio/mixertui x11-fonts/nerd-fonts x11-drivers/xf86-video-intel 
 
 # This is for better intel graphics
 add kern.vty=vt to /boot/loader.conf
+
+# This is for intel graphics look at https://wiki.freebsd.org/Graphics for other settings
+sysrc -f /etc/rc.conf kld_list+=i915kms
 
 # Adding bash as default shell for my user
 chsh -s /usr/local/bin/bash will
