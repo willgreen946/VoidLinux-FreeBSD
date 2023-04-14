f9b83f395bf1a5c2b58c0d180b04144f29238705
ZK <72352225+willrollitt@users.noreply.github.com>
2023-04-14 13:25:13 +0100

    removed nerd fonts as it was causing issues

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Installing all packages I use
-exec pkg install doas vim vifm lynx git xorg neofetch scrot drm-kmod font-awesome feh ungoogled-chromium mpv wifimgr bash zathura ubuntu-font picom audio/mixertui x11-fonts/nerd-fonts x11-drivers/xf86-video-intel 
+exec pkg install doas vim vifm lynx git xorg neofetch scrot drm-kmod font-awesome feh ungoogled-chromium mpv wifimgr bash zathura ubuntu-font picom audio/mixertui  x11-drivers/xf86-video-intel 
 
 # This is for better intel graphics
 add kern.vty=vt to /boot/loader.conf
