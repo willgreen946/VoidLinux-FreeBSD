4b7c8f2b7db300c8e7a394498bc7683cad9409b2
ZK <72352225+willrollitt@users.noreply.github.com>
2023-04-16 14:30:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 # Installing all packages I use
-exec pkg install doas vim vifm lynx git xorg neofetch scrot drm-kmod automake autoconf autotools font-awesome feh ungoogled-chromium mpv wifimgr bash zathura ubuntu-font picom audio/mixertui  x11-drivers/xf86-video-intel 
+exec pkg install doas vim vifm lynx git neofetch scrot drm-kmod automake autoconf autotools font-awesome ungoogled-chromium bash zathura ubuntu-font audio/mixertui  x11-drivers/xf86-video-intel 
+
+# wayland packages
+exec pkg install wayland seatd 
+
+# Run for wayland support
+exec sysrc seatd_enable=”YES”
+exec service seatd start
+
+# x11 packages uncomment for x11
+# exec pkg install xorg wifimgr feh mpv picom
 
 # This is for better intel graphics
 add kern.vty=vt to /boot/loader.conf
