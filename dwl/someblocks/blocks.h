7f4d0c742e600d80b840ac898cd0e0091dbe3d9a
ZK <72352225+willrollitt@users.noreply.github.com>
2023-04-16 23:13:05 +0100

    Fixed issue where CPU usage would max

--- dwl/someblocks/blocks.h
+++ dwl/someblocks/blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"[ FreeBSD 13.1]", 0 ,0 },
-	{"[  ", "echo $(apm -l)%]",	30,		0},
-	{"[", "date '+%a %b %d %I:%M]'",		5,		0},
+	{"[ FreeBSD 13.1]", 0,1},
+	{"[  ", "echo $(apm -l)%]",	60,		1},
+	{"[ ", "date '+%a %b %d %I:%M]'",		60,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
