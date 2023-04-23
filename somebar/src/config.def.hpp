50242aaf683cff4b92fe4b160beab4de538133ce
ZK <72352225+africavoid@users.noreply.github.com>
2023-04-23 17:45:11 +0000

    new and cleaner builds

---
+++ somebar/src/config.def.hpp
@@ -0,0 +1,26 @@
+// somebar - dwl bar
+// See LICENSE file for copyright and license details.
+
+#pragma once
+#include "common.hpp"
+
+constexpr bool topbar = true;
+
+constexpr int paddingX = 8;
+constexpr int paddingY = 1;
+
+// See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
+constexpr const char* font = "ubuntu mono 10";
+
+constexpr ColorScheme colorInactive = {Color(0xeb, 0xdb, 0xb2), Color(0x28, 0x28, 0x28)};
+constexpr ColorScheme colorActive = {Color(0xeb, 0xdb, 0xb2), Color(0x45, 0x85, 0x88)};
+constexpr const char* termcmd[] = {"foot", nullptr};
+
+static std::vector<std::string> tagNames = {
+	 "", "", "",
+         "", "", "",
+};
+
+constexpr Button buttons[] = {
+	{ ClkStatusText,   BTN_RIGHT,  spawn,      {.v = termcmd} },
+};
